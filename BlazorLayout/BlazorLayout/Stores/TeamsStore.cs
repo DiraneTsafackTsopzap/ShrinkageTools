19123f5aabdb2a2e943de7b7b987eda0fafe950e
DiraneTsafackTsopzap <diraneserges@gmail.com>
2026-01-05 12:57:48 +0100

    Explication du Store TeamsStore

--- BlazorLayout/BlazorLayout/Stores/TeamsStore.cs
+++ BlazorLayout/BlazorLayout/Stores/TeamsStore.cs
@@ -11,9 +11,13 @@ public sealed partial class TeamsStore : StoreBase
 
     public void InitializeTeams(IReadOnlyList<TeamDto> teams)
     {
+        // Console.WriteLine($"__Teams count Before Initialisation = {__Teams.Count}"); // visible dans la console navigateur
+
         if (__Teams.Any()) throw new InvalidOperationException("Teams were already been initialized.");
 
         Teams = teams;
+
+       //  Console.WriteLine($"__Teams count after Initialisation = {__Teams.Count}");
     }
 
     public void Reset()
@@ -24,3 +28,12 @@ public sealed partial class TeamsStore : StoreBase
 
 // Readme Important : Le AutoSubscribe generer ici provient du BlazorAnalyer et dans le StoreAutoSubscribeGenerator.cs
 // S'asssurr que le Nom est BlazorLayout.StateManagement.AutoSubscribeAttribute 
+
+// Teams.Any() : La liste contient t'elle des elements ?  
+//  List<int> numbers = new List<int> { 1, 2, 3 };
+//  bool hasElements = numbers.Any(); // true
+//  List<int> emptyNumbers = new List<int>();
+//  bool hasNoElements = emptyNumbers.Any(); // false
+
+// Ce Store permet de Stocker la liste des equipes (Teams) dans l'application BlazorLayout.
+
