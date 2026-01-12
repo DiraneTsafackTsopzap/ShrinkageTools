b2100c5a8b404f207dcd0af6ed0fed895f1f5942
Dirane Tsafack Tsopzap <dirane.tsafack@lowellgroup.de>
2026-01-12 13:56:39 +0100

    01- Apprentissage du c#

---
+++ TrainingConsoleC#/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+/// <summary>
+///     Ceci est la Version 12.0 de C#
+/// </summary>
+//Console.WriteLine("Hello, World!");
+
+
+/// <summary>
+///     Ceci est l'ancienne version de C#
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This is a sample C# 12.0 program.");
+    }
+}
