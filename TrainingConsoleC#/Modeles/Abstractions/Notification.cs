92f21fbe1fce62151fb01b6e4bfc23f7abb9063a
Dirane Tsafack <Dirane.Tsafack@lowellgroup.de>
2026-01-13 17:23:51 +0100

    09 - Course on Abstractions

---
+++ TrainingConsoleC#/Modeles/Abstractions/Notification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingConsoleC_.Modeles.Abstractions
+{
+    public abstract class Notification
+    {
+        ///<summary>
+        /// Une classe (ou interface) où on définit une méthode sans l’écrire.
+        /// Cette méthode est un contrat.
+        /// Les autres classes doivent respecter ce contrat.
+        /// </summary>
+
+
+        public abstract void SendNotification(string message);
+
+
+
+
+    }
+}
